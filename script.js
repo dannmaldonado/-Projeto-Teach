cc5deaec216ff3f401949a954f07eb4a9d1a2035
Danilo Maldonado <dannsanto@gmail.com>
2022-06-04 12:22:06 -0300

    correção JS

--- script.js
+++ script.js
@@ -64,3 +64,4 @@ document.addEventListener("DOMContentLoaded", function(){
     alert(`Obrigado por informar seu telefone  ${mask.unmaskedValue} no formato correto: ${mask.value}`);
     });
 });
+};
