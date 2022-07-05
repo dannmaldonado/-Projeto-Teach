b87c5db9e158bd3f7d42202d5af55fddb7a952f5
Danilo Maldonado <dannsanto@gmail.com>
2022-07-04 23:52:37 -0300

    Form com msg de envio

--- script.js
+++ script.js
@@ -1,66 +1,75 @@
-function fncValida(){
+function fncValida() {
 
-//encontrar o elemento na arvore DOM pelo ID e atribuir o valor do mesmo a uma variável  
+    //encontrar o elemento na arvore DOM pelo ID e atribuir o valor do mesmo a uma variável  
 
-let nome = document.querySelector("#txtNome").value;
-let email = document.forms[0].txtEmail.value;
-let fone = document.querySelector("txtFone");
-let uf = document.querySelector("#ddlUF");
-let cidade = document.querySelector("txtCidade");
-let motivo = document.querySelector("txtMotivo");
-let mensagem = document.querySelector("txtMensagem");
+    let nome = document.querySelector("#txtNome").value;
+    let email = document.forms[0].txtEmail.value;
+    let fone = document.querySelector("txtFone");
+    let uf = document.querySelector("#ddlUF");
+    let cidade = document.querySelector("txtCidade");
+    let motivo = document.querySelector("txtMotivo");
+    let mensagem = document.querySelector("txtMensagem");
 
-//se a variável não tem um texto preenchido
-if(nome == ""){
-    alert('Por Favor, Preencha o seu nome');
-    return false;
-}
-
-  //se a variável não tem um texto preenchido
-if(email == ""){
-    alert('Por Favor, Preencha o email');
-    return false;
-}
-
-  //se a variável não tem um texto preenchido
-if(fone == ""){
-    alert('Por Favor, Preencha o telefone');
-    return false;
-}
-  //se a variável não tem um texto preenchido
-if(uf.selectedIndex==0){
-    alert('Por Favor, Selecione o estado (UF)');
-    return false;
-}
     //se a variável não tem um texto preenchido
-if(cidade == ""){
-    alert('Por Favor, Preencha sua cidade');
-    return false;
-}
-//se a variável não tem um texto preenchido
-if(motivo == ""){
+    if (nome == "") {
+        alert('Por Favor, Preencha o seu nome');
+        return false;
+    }
+
+    //se a variável não tem um texto preenchido
+    if (email == "") {
+        alert('Por Favor, Preencha o email');
+        return false;
+    }
+
+    //se a variável não tem um texto preenchido
+    if (fone == "") {
+        alert('Por Favor, Preencha o telefone');
+        return false;
+    }
+    //se a variável não tem um texto preenchido
+    if (uf.selectedIndex == 0) {
+        alert('Por Favor, Selecione o estado (UF)');
+        return false;
+    }
+    //se a variável não tem um texto preenchido
+    if (cidade == "") {
+        alert('Por Favor, Preencha sua cidade');
+        return false;
+    }
+    //se a variável não tem um texto preenchido
+    if (motivo == "") {
         alert('Por Favor, Preencha o motivo do seu contato');
         return false;
-}
-//se a variável não tem um texto preenchido
-if(mensagem == ""){
-    alert('Por Favor, Preencha como podemos te ajudar');
-    return false;
-}
+    }
+    //se a variável não tem um texto preenchido
+    if (mensagem == "") {
+        alert('Por Favor, Preencha como podemos te ajudar');
+        return false;
+    }
 };
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     let btnEnviar = document.querySelector("#btnEnviar");
-    btnEnviar.addEventListener("click", function(){
-        fncValida();
+    btnEnviar.addEventListener("click", function () {
+        fncValida();      
+        alert('Formulário enviado com sucesso!');
+        
+        // tentando salvar os valores digitados no input no console.log // 
+
+        // let cadastro = "";
+        // cadastro = document.getElementById("input#formContato").value;
+        // console.log(cadastro);
     });
-    
+
+
+
     var telMask = document.getElementById('txtFone');
     var maskOptions = {
-    mask: '+{55}(00)00000-0000'
+        mask: '+{55}(00)00000-0000'
     };
     var mask = IMask(telMask, maskOptions);
-    
+
     // mask.on("complete", function () {
     // alert(`Obrigado por informar seu telefone  ${mask.unmaskedValue} no formato correto: ${mask.value}`);
     // });
