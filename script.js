5a6b65573acb21b609b7a86f22028b4b1eac2f32
Danilo Maldonado <dannsanto@gmail.com>
2022-07-05 23:43:06 -0300

    Js jQuery

--- script.js
+++ script.js
@@ -27,42 +27,50 @@ function fncValida() {
         alert('Por Favor, Preencha o telefone');
         return false;
     }
+
     //se a variável não tem um texto preenchido
     if (uf.selectedIndex == 0) {
         alert('Por Favor, Selecione o estado (UF)');
         return false;
     }
+
     //se a variável não tem um texto preenchido
     if (cidade == "") {
         alert('Por Favor, Preencha sua cidade');
         return false;
     }
+
     //se a variável não tem um texto preenchido
     if (motivo == "") {
         alert('Por Favor, Preencha o motivo do seu contato');
         return false;
     }
+
     //se a variável não tem um texto preenchido
     if (mensagem == "") {
         alert('Por Favor, Preencha como podemos te ajudar');
         return false;
     }
+
+    else {
+        return true;
+    }
+    
 };
 
 document.addEventListener("DOMContentLoaded", function () {
     let btnEnviar = document.querySelector("#btnEnviar");
     btnEnviar.addEventListener("click", function () {
-        fncValida();      
-        alert('Formulário enviado com sucesso!');
+        fncValida();
+        if (false !== true){
+            alert('Formulário enviado com sucesso!');
+        }
         
-        // tentando salvar os valores digitados no input no console.log // 
-
-        // let cadastro = "";
-        // cadastro = document.getElementById("input#formContato").value;
-        // console.log(cadastro);
     });
 
-
+    // btnEnviar.addEventListener("click", function(e) {
+    //     e.preventDefault();
+    // });
 
     var telMask = document.getElementById('txtFone');
     var maskOptions = {
@@ -74,5 +82,36 @@ document.addEventListener("DOMContentLoaded", function () {
     // alert(`Obrigado por informar seu telefone  ${mask.unmaskedValue} no formato correto: ${mask.value}`);
     // });
 });
+
+function showValues() {
+    var str = $("form").serialize();
+    $("#results").text(str);
+}
+$("input[type='checkbox'], input[type='radio']").on("click", showValues);
+$("select").on("change", showValues);
+showValues();
 // Comentário
 
+// const btn = document.querySelector("#btnEnviar");
+
+// btn.addEventListener("click", function(e) {
+//     e.preventDefault();
+
+//     const email = document.querySelector("#email");
+//     // const email = document.querySelector("#txtEmail");
+//     // const fone = document.querySelector("txtFone");
+//     // const uf = document.querySelector("#ddlUF");
+//     // const cidade = document.querySelector("txtCidade");
+//     // const motivo = document.querySelector("txtMotivo");
+//     // const mensagem = document.querySelector("txtMensagem");
+
+//     const value = email.value;
+//     //[nome.value, email.value, fone.value, uf.value, cidade.value, motivo.value, mensagem.value];
+//     console.log(value);
+// })
+
+// var telMask = document.getElementById('txtFone');
+// var maskOptions = {
+//     mask: '+{55}(00)00000-0000'
+// };
+// var mask = IMask(telMask, maskOptions);
